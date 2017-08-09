b10234704ab9c479530f8a353716c85577b869f0
root <root@lms2.learndata.info>
2017-08-09 15:40:41 +0000

    Adding update stats cron

--- web/_includes/config.inc.php
+++ web/_includes/config.inc.php
@@ -1,18 +1,18 @@
 <?php
-	$connection_url = getenv("MONGOLAB_URI");
+//	$connection_url = "mongodb://admin:OpenData2012@ds029778.mlab.com:29778/elearning-test";
+	$connection_url = "mongodb://heroku_0vr2zs59:ctgtfnb5kgrc9gbl0a6etdk9o8@ds055792.mongolab.com:55792/heroku_0vr2zs59";
 	$url = parse_url($connection_url);
-	$db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
-	$collection = getenv("MONGOLAB_COLLECTION");
-	$mandrill_key = getenv("MANDRILL_KEY");
-	$eLearning_prefix = getenv("ELEARNING_PREFIX");
-    $client_id = getenv("GOOGLE_OAUTH_ID");
- 	$client_secret = getenv("GOOGLE_OAUTH_SECRET");
- 	$redirect_uri = getenv("GOOGLE_REDIRECT_URI");
- 	$drive_client_id = getenv("ODI_DRIVE_CLIENTID");
- 	$drive_client_secret = getenv("ODI_DRIVE_CLIENTSECRET");
- 	$drive_token = getenv("ODI_DRIVE_TOKEN");
- 	$collection = "elearning";
- 	$courses_collection = "courses";
+	//$db_name = 'elearning-test';
+	$db_name = 'heroku_0vr2zs59';
+	$collection = "elearning";
+	$courses_collection = "courses";
 	$instances_collection = "instances";
-	$exec_path = "~";
+        $mandrill_key = "S20afJPHdCR6p0kjrEw9JA";
+	$client_id = "112022861718-3dojeucmk642cd4hgtnojq9r76dpajg1.apps.googleusercontent.com";
+ 	$client_secret = "J4diuIIPi0fcSgYe3SRWg5Fw";
+ 	$drive_client_id = "683890023722-4h3trn3d6o3705nq6d9duoo78r546pmj.apps.googleusercontent.com";
+ 	$drive_client_secret = "GzpYCGE18i2aflsSSYoQyFRW";
+ 	$drive_token = '{"access_token":"ya29.bwLc9w_V5t2t9eifOO5zPZSsah-ppR0aePGlkDAKZ70uSxFYCdT9wbI_ReCIXI3rS-xc","token_type":"Bearer","expires_in":3600,"refresh_token":"1\/06zDE_OQV6LWXJmrlqbGWwV8rPymGXyjskjpD2uA3k1IgOrJDtdun6zK6XiATCKT","created":1453287197}';
+ 	$redirect_path = "";
+ 	$exec_path = "/var/www/";
 ?>
