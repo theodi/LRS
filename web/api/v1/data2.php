f544c6e787698ed17bb987275d17d710fcf7afa0
Dave Tarrant <davetaz@theodi.org>
2018-01-26 12:27:46 +0000

    try again

--- web/api/v1/data2.php
+++ web/api/v1/data2.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_WARNING);
+error_reporting(E_ALL ^ E_WARNING);
 // TRY THIS VERSION
 $debug = false;
 //$debugid = "q@q.com";
