74bbf4111edfc0f4c17a287250cab2e5ded96880
Dave Tarrant <davetaz@theodi.org>
2018-07-13 14:06:08 +0100

    Updating for legacy trackingHub

--- web/api/v2/store.php
+++ web/api/v2/store.php
@@ -14,30 +14,141 @@ if ($_SERVER["HTTP_HOST"] == "localhost") {
 
 require_once('library/sendMail.php');
 
+function getCourseID($url) {
+  global $courseIDs;
+  if ($courseIDs[$url]) {
+    return $courseIDs[$url];
+  }
+  if (substr($url, 0,4) != "http") {
+    $geturl = "http://" . $url . "/course/config.json";
+  } else {
+    $geturl = $url . "/course/config.json";
+  }
+
+  $contents = file_get_contents($geturl);
+  $json = json_decode($contents,true);
+  if ($json["_id"]) {
+    $courseIDs[$url] = $json["_id"];
+    return $json["_id"];
+  } else {
+    $courseIDs[$url] = str_replace(".","_",$url);
+    return str_replace(".","_",$url);
+  }
+}
+
+function handleLegacy($data) {
+
+  $data["progress"]["_trackingHubID"] = $data["progress"]["courseID"];
+  $data["progress"]["courseID"] = getCourseID($data["progress"]["_trackingHubID"]);
+
+  $data["progress"]["_isComplete"] = true;
+  $count = 0;
+  $overall = 0;
+  foreach($data["progress"] as $id => $moduleData) {
+    if(is_array($moduleData)) {
+      if ($moduleData["progress"] > 99 || $moduleData["_isComplete"] == true || $moduleData["answers"]["_assessmentState"] == "Passed" || $moduleData["answers"]["_assessmentState"] == "Failed") {
+        $overall += 100;
+      } elseif(is_numeric($moduleData["progress"])) {
+        $overall += $moduleData["progress"];
+        $data["progress"]["_isComplete"] = false;
+      } else {
+        $data["progress"]["_isComplete"] = false;
+      }
+      $count++;
+      if (!$data["progress"]["lang"] && $moduleData["lang"]) {
+        $data["progress"]["lang"] = $moduleData["lang"];
+      }
+      unset($moduleData["lang"]);
+      if (!$data["progress"]["theme"] && $moduleData["theme"]) {
+        $data["progress"]["theme"] = $moduleData["theme"];
+      }
+      unset($moduleData["theme"]);
+      $data["progress"][$id] = $moduleData;
+    }
+  }
+  $data["progress"]["progress"] = round($overall / $count);
+  syslog(100, "overall : " . $overall . " / " . $count);
+  $count = 0;
+  if ($data["progress"]["_isComplete"] || $data["progress"]["answers"]["_assessmentState"]) {
+    $assessmentData["isComplete"] = true;
+    $assessmentData["isPercentageBased"] = true;
+    $assessmentData["isPass"] = true;
+    foreach($data["progress"] as $id => $moduleData) {
+      if (@is_array($moduleData["answers"])) {
+        $localData = [];
+        $localData["id"] = "assessment" . $count;
+        $localData["isComplete"] = true;
+        $localData["attempts"] = 1;
+        $localData["isPass"] = false;
+        if ($data["progress"]["answers"]["_assessmentState"] == "Passed") {
+          $localData["isPass"] = true;
+        }
+        $assessmentData["assessments"]++;
+        $answerData = $moduleData["answers"];
+        if ($answerData["_assessmentState"]) {
+          $assessmentData["assessmentsComplete"]++;
+          if ($answerData["_assessmentState"] != "Passed") {
+            $assessmentData["isPass"] = false;
+          }
+        }
+        foreach ($answerData as $key => $value) {
+          if(is_array($answerData[$key])) {
+            $localData["maxScore"]++;
+            $assessmentData["maxScore"]++;
+            if ($answerData[$key]["_isCorrect"]) {
+              $localData["score"]++;
+              $assessmentData["score"]++;
+            }
+          }
+        }
+        $localData["scoreAsPercent"] = round(($localData["score"] / $localData["maxScore"]) * 100);
+        if ($moduleData["answers"]["_assessmentState"]) {
+          $localData["_assessmentState"] = $moduleData["answers"]["_assessmentState"];
+          unset($data["progress"][$id]["answers"]["_assessmentState"]);
+        }
+        $data["progress"][$id]["assessments"]["assessment" . $count] = $localData;
+        $count++;
+      }
+    }
+    $assessmentData["scoreAsPercent"] = round(($assessmentData["score"] / $assessmentData["maxScore"]) * 100);
+    $data["assessments"] = $assessmentData;
+  }
+  return $data;
+}
+
 function store($data,$courseID) {
-   global $connection_url, $db_name;
+
+	 global $connection_url, $db_name;
    $collection = "adapt2";
    $id = $data["user"]["id"];
    $overall = 0; $count = 0;
-   $data["progress"]["_isComplete"] = true;
+   //$data["progress"]["_isComplete"] = true;
    foreach($data["progress"] as $module => $progress) {
    	 if (!is_array($progress)) {
    	 	continue;
    	 }
-   	 if ($progress["_isComplete"] == false) {
-		$data["progress"]["_isComplete"] = false;
-	 }
+   	 //if ($progress["_isComplete"] == false) {
+		 // $data["progress"]["_isComplete"] = false;
+	   //}
    	 $overall = $overall + $progress["progress"]; 
    	 $count = $count + 1;
    	 if (!$courseID || $courseID == "") {
      	$courseID = $progress["courseID"];
      }
    }
-   $data["progress"]["progress"] = round($overall / $count);
-   if (!$data["progress"]["courseID"]) {
-   	$data["progress"]["courseID"] = $courseID;
-   }
-   $courseID = str_replace(".", "_", $courseID);
+   
+    if (!$data["progress"]["courseID"]) {
+      $data["progress"]["courseID"] = $courseID;
+    }
+    $courseID = getCourseID($courseID);
+
+   //Handle legacy trackingHub
+   if (!$data["progress"]["theme"]) {
+      $data = handleLegacy($data);
+    }
+
+    
+
    try {
    	if ($courseID) {
    		$toSet[$courseID] = $data;
@@ -76,9 +187,9 @@ function store($data,$courseID) {
 
 	$m->close();
 
-	if (!getMailLock(2)) {
-		findEmailsCollection("adapt2",2);
-	}
+#	if (!getMailLock(2)) {
+#		findEmailsCollection("adapt2",2);
+#	}
 
 	return true;
    } catch ( Exception $e ) {
